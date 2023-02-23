4a5cbd3a4a47682b4fd07920f9d18e523214d891
RudiJr <rudi.juniior@gmail.com>
2023-02-22 23:19:18 -0300

    modifying the item value with js

--- js/main.js
+++ js/main.js
@@ -1,46 +1,58 @@
-const form = document.getElementById("novoItem") 
-const lista = document.getElementById("lista")
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
 /* Getting the value of the localStorage item "itens" and parsing it into an array. If there is no
 value, it will return an empty array. */
-const itens = JSON.parse(localStorage.getItem("itens")) || []  
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
 /* Iterating over the array `itens` and calling the function `criaElemento` for each element. */
 itens.forEach( (elemento) => {    
-    criaElemento(elemento)
-} )
+    criaElemento(elemento);
+} );
 
 form.addEventListener("submit", (evento) => {  
-    evento.preventDefault()
+    evento.preventDefault();
 
-    const nome = evento.target.elements['nome']
-    const quantidade = evento.target.elements['quantidade']
-
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+    const existe = itens.find(elemento => elemento.nome === nome.value);
+    
     const itemAtual = {
-    "nome": nome.value,
-    "quantidade": quantidade.value
+        "nome": nome.value,
+        "quantidade": quantidade.value
+        };
+
+    if(existe){
+        itemAtual.id = existe.id;
+        atualizaElemento(itemAtual);
+    }else{
+        itemAtual.id = itens.length;
+
+        criaElemento(itemAtual);
+        itens.push(itemAtual);
     }
 
-    criaElemento(itemAtual)
-
-    itens.push(itemAtual)
-
    /* Saving the array `itens` into the localStorage item "itens". */
-    localStorage.setItem("itens", JSON.stringify(itens))
+    localStorage.setItem("itens", JSON.stringify(itens));
 
-    nome.value = ""
-    quantidade.value = ""
+    nome.value = "";
+    quantidade.value = "";
     
 })
 
 function criaElemento(item) {  
-    const novoItem = document.createElement('li')
-    novoItem.classList.add("item")
+    const novoItem = document.createElement('li');
+    novoItem.classList.add("item");
 
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = item.quantidade
-    novoItem.appendChild(numeroItem)
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id;
 
-    novoItem.innerHTML += item.nome
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += item.nome;
 
-    lista.appendChild(novoItem)
-}
+    lista.appendChild(novoItem);
+};
+
+function atualizaElemento(item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+};
